48f0ebe6bd44fef81481816f46efd6bfb45e0588
Foti Dim <fdimanidis@gmail.com>
2024-07-12 18:27:51 +0200

    Minor changes

--- example/lib/home_controller.dart
+++ example/lib/home_controller.dart
@@ -12,9 +12,14 @@ class HomeController extends GetxController {
   RxBool isBleOn = false.obs;
   RxList<String> devices = <String>[].obs;
 
-  String deviceName = "BTR-C";
+  String deviceName = "BJMA23095";
 
-  String controlService = "e635bed3-69af-4347-99fa-d6f34b791169";
+  // Remote o
+
+  String controlService = "a33fb7b9-e50c-cc6c-4811-cd6549ece898";
+  String controlService2 = "e635bed3-69af-4347-99fa-d6f34b791169";
+  // String controlService2 = "adc505f9-4e58-4b71-b8ca-983bb8c73e4f";
+
   String controlChar1 = "82f949b4-f5dc-4cf3-ab3c-fd9fd4017b68";
   String controlChar2 = "b7a8015c-cb94-4efa-bda2-b7921fa9951f";
   String controlChar3 = "05a02050-0860-4919-8add-9801fba8b6ed";
@@ -80,9 +85,7 @@ class HomeController extends GetxController {
   void startAdvertising() async {
     print("Starting Advertising");
     await BlePeripheral.startAdvertising(
-      services: [
-        // TODO: add advertising service
-      ],
+      services: [controlService2],
       localName: deviceName,
     );
   }
@@ -90,90 +93,91 @@ class HomeController extends GetxController {
   void addServices() async {
     try {
       // Add General Services
-      if (!Platform.isWindows) {
-        // Add 0x1800 Service
-        await BlePeripheral.addService(
-          BleService(
-            uuid: "00001800-0000-1000-8000-00805f9b34fb",
-            primary: true,
-            characteristics: [
-              BleCharacteristic(
-                uuid: "00002A00-0000-1000-8000-00805f9b34fb",
-                properties: [CharacteristicProperties.read.index],
-                permissions: [AttributePermissions.readable.index],
-                value: Uint8List.fromList([0x42, 0x54, 0x52, 0x2D, 0x46, 0x31]),
-              ),
-              BleCharacteristic(
-                uuid: "00002A01-0000-1000-8000-00805f9b34fb",
-                properties: [CharacteristicProperties.read.index],
-                permissions: [AttributePermissions.readable.index],
-                value: Uint8List.fromList([0x00, 0x00]),
-              ),
-            ],
-          ),
-        );
+      // if (!Platform.isWindows) {
+      //   // Add 0x1800 Service
+      //   await BlePeripheral.addService(
+      //     BleService(
+      //       uuid: "00001800-0000-1000-8000-00805f9b34fb",
+      //       primary: true,
+      //       characteristics: [
+      //         BleCharacteristic(
+      //           uuid: "00002A00-0000-1000-8000-00805f9b34fb",
+      //           properties: [CharacteristicProperties.read.index],
+      //           permissions: [AttributePermissions.readEncryptionRequired.index],
+      //           value: Uint8List.fromList([0x42, 0x54, 0x52, 0x2D, 0x46, 0x31]),
+      //         ),
+      //         BleCharacteristic(
+      //           uuid: "00002A01-0000-1000-8000-00805f9b34fb",
+      //           properties: [CharacteristicProperties.read.index],
+      //           permissions: [AttributePermissions.readEncryptionRequired.index],
+      //           value: Uint8List.fromList([0x00, 0x00]),
+      //         ),
+      //       ],
+      //     ),
+      //   );
 
-        // Add 0x1801 Service
-        await BlePeripheral.addService(
-          BleService(
-            uuid: "00001801-0000-1000-8000-00805f9b34fb",
-            primary: true,
-            characteristics: [
-              BleCharacteristic(
-                uuid: "00002A05-0000-1000-8000-00805f9b34fb",
-                properties: [CharacteristicProperties.indicate.index],
-                permissions: [AttributePermissions.readable.index],
-                descriptors: [
-                  BleDescriptor(
-                    uuid: "00002902-0000-1000-8000-00805f9b34fb",
-                    value: Uint8List.fromList([0x00, 0x00]),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        );
+      //   // Add 0x1801 Service
+      //   await BlePeripheral.addService(
+      //     BleService(
+      //       uuid: "00001801-0000-1000-8000-00805f9b34fb",
+      //       primary: true,
+      //       characteristics: [
+      //         BleCharacteristic(
+      //           uuid: "00002A05-0000-1000-8000-00805f9b34fb",
+      //           properties: [CharacteristicProperties.indicate.index],
+      //           permissions: [AttributePermissions.readEncryptionRequired.index],
+      //           descriptors: [
+      //             // BleDescriptor(
+      //             //   uuid: "00002902-0000-1000-8000-00805f9b34fb",
+      //             //   value: Uint8List.fromList([0x00, 0x00]),
+      //             // ),
+      //           ],
+      //         ),
+      //       ],
+      //     ),
+      //   );
 
-        // Add 0x180A Service
-        await BlePeripheral.addService(
-          BleService(
-            uuid: "0000180a-0000-1000-8000-00805f9b34fb",
-            primary: true,
-            characteristics: [
-              // Manufacturer Name
-              BleCharacteristic(
-                uuid: "00002a29-0000-1000-8000-00805f9b34fb",
-                properties: [CharacteristicProperties.read.index],
-                permissions: [AttributePermissions.readable.index],
-                value: Uint8List.fromList(
-                    [0x46, 0x55, 0x4A, 0x49, 0x46, 0x49, 0x4C, 0x4D, 0x00]),
-              ),
-              // Model Number
-              BleCharacteristic(
-                uuid: "00002a24-0000-1000-8000-00805f9b34fb",
-                properties: [CharacteristicProperties.read.index],
-                permissions: [AttributePermissions.readable.index],
-                value: Uint8List.fromList(
-                    [0x42, 0x54, 0x52, 0x2D, 0x46, 0x31, 0x00]),
-              ),
-              // Serial Number
-              BleCharacteristic(
-                uuid: "00002a27-0000-1000-8000-00805f9b34fb",
-                properties: [CharacteristicProperties.read.index],
-                permissions: [AttributePermissions.readable.index],
-                value: Uint8List.fromList([0x31, 0x2E, 0x31, 0x00]),
-              ),
-              // Firmware Revision
-              BleCharacteristic(
-                uuid: "00002a26-0000-1000-8000-00805f9b34fb",
-                properties: [CharacteristicProperties.read.index],
-                permissions: [AttributePermissions.readable.index],
-                value: Uint8List.fromList([0x31, 0x2E, 0x31, 0x00]),
-              ),
-            ],
-          ),
-        );
-      }
+      //   // Add 0x180A Service
+      //   await BlePeripheral.addService(
+      //     BleService(
+      //       uuid: "0000180a-0000-1000-8000-00805f9b34fb",
+      //       primary: true,
+      //       characteristics: [
+      //         // Manufacturer Name
+      //         BleCharacteristic(
+      //           uuid: "00002a29-0000-1000-8000-00805f9b34fb",
+      //           properties: [CharacteristicProperties.read.index],
+      //           permissions: [AttributePermissions.readEncryptionRequired.index],
+      //           value: Uint8List.fromList(
+      //               [0x46, 0x55, 0x4A, 0x49, 0x46, 0x49, 0x4C, 0x4D, 0x00]),
+      //         ),
+      //         // Model Number
+      //         BleCharacteristic(
+      //           uuid: "00002a24-0000-1000-8000-00805f9b34fb",
+      //           properties: [CharacteristicProperties.read.index],
+      //           permissions: [AttributePermissions.readEncryptionRequired.index],
+      //           value: Uint8List.fromList(
+      //               [0x42, 0x54, 0x52, 0x2D, 0x46, 0x31, 0x00]),
+      //         ),
+      //         // Serial Number
+      //         BleCharacteristic(
+      //           uuid: "00002a27-0000-1000-8000-00805f9b34fb",
+      //           properties: [CharacteristicProperties.read.index],
+      //           permissions: [AttributePermissions.readEncryptionRequired.index],
+      //           value: Uint8List.fromList([0x31, 0x2E, 0x31, 0x00]),
+      //         ),
+      //         // Firmware Revision
+      //         BleCharacteristic(
+      //           uuid: "00002a26-0000-1000-8000-00805f9b34fb",
+      //           properties: [CharacteristicProperties.read.index],
+      //           permissions: [AttributePermissions.readEncryptionRequired.index],
+      //           value: Uint8List.fromList([0x31, 0x2E, 0x31, 0x00]),
+      //         ),
+      //       ],
+      //     ),
+      //   );
+
+      // }
 
       // Add Control Service
       await BlePeripheral.addService(
@@ -189,8 +193,8 @@ class HomeController extends GetxController {
                 CharacteristicProperties.notify.index,
               ],
               permissions: [
-                AttributePermissions.writeable.index,
-                AttributePermissions.readable.index
+                AttributePermissions.writeEncryptionRequired.index,
+                AttributePermissions.readEncryptionRequired.index
               ],
             ),
             BleCharacteristic(
@@ -201,8 +205,8 @@ class HomeController extends GetxController {
                 CharacteristicProperties.notify.index,
               ],
               permissions: [
-                AttributePermissions.writeable.index,
-                AttributePermissions.readable.index
+                AttributePermissions.writeEncryptionRequired.index,
+                AttributePermissions.readEncryptionRequired.index
               ],
             ),
             BleCharacteristic(
@@ -213,8 +217,53 @@ class HomeController extends GetxController {
                 CharacteristicProperties.notify.index,
               ],
               permissions: [
-                AttributePermissions.writeable.index,
-                AttributePermissions.readable.index,
+                AttributePermissions.writeEncryptionRequired.index,
+                AttributePermissions.readEncryptionRequired.index,
+              ],
+            ),
+          ],
+        ),
+      );
+
+      await BlePeripheral.addService(
+        BleService(
+          uuid: controlService2,
+          primary: true,
+          characteristics: [
+            BleCharacteristic(
+              uuid: controlChar1,
+              properties: [
+                CharacteristicProperties.read.index,
+                CharacteristicProperties.write.index,
+                CharacteristicProperties.notify.index,
+              ],
+              permissions: [
+                AttributePermissions.writeEncryptionRequired.index,
+                AttributePermissions.readEncryptionRequired.index
+              ],
+            ),
+            BleCharacteristic(
+              uuid: controlChar2,
+              properties: [
+                CharacteristicProperties.read.index,
+                CharacteristicProperties.write.index,
+                CharacteristicProperties.notify.index,
+              ],
+              permissions: [
+                AttributePermissions.writeEncryptionRequired.index,
+                AttributePermissions.readEncryptionRequired.index
+              ],
+            ),
+            BleCharacteristic(
+              uuid: controlChar3,
+              properties: [
+                CharacteristicProperties.read.index,
+                CharacteristicProperties.write.index,
+                CharacteristicProperties.notify.index,
+              ],
+              permissions: [
+                AttributePermissions.writeEncryptionRequired.index,
+                AttributePermissions.readEncryptionRequired.index,
               ],
             ),
           ],
